ba623f3824d324175727b091530850107b49b676
Braian Manrique <braianm@iadb.org>
2024-07-16 17:34:07 -0500

    add headers

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { RouterModule } from '@angular/router';
+import { MaterialModule } from '../material/material.module';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    SidebarComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MaterialModule
+  ],
+  exports:[
+    SidebarComponent,
+    HeaderComponent
+  ]
+})
+export class SharedModule { }
